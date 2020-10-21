e2a435e5dc083705a5f1f7de5d90b2d458f52bcb
ptmew <pawee.tanti@gmail.com>
2020-10-22 01:18:31 +0700

    backend api for argo service

---
+++ backend/src/utils/argoApi.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+export const startArgo = async (method, url, queryData) => {
+  const headers = {};
+  headers['Content-Type'] = 'application/json';
+  try {
+    const response = await axios({
+      method,
+      url,
+      data: JSON.stringify(queryData),
+      headers,
+    });
+    return response.data;
+  } catch (err) {
+    return err;
+  }
+};
