68cad4b11f947811e51bfe2cc2e53247853b8e2b
ptmew <pawee.tanti@gmail.com>
2020-11-02 17:50:42 +0700

    fixed

--- backend/src/utils/googleValidate.js
+++ backend/src/utils/googleValidate.js
@@ -1,6 +1,6 @@
 import { OAuth2Client } from 'google-auth-library';
 
-const CLIENT_ID = '123';
+const CLIENT_ID = '647735027802-876e7ib9pi85sbtcq2sa626c1rigcnqi.apps.googleusercontent.com';
 const client = new OAuth2Client(CLIENT_ID);
 
 export const verifyGoogleToken = async (token) => {
