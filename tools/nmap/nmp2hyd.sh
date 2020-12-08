d5525a3eb829dd3230cbc915006e69f597629318
soponpakorn <60011271@kmitl.ac.th.com>
2020-12-08 22:37:21 +0700

    minor fix

--- tools/nmap/nmp2hyd.sh
+++ tools/nmap/nmp2hyd.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-fname="nmap.txt"
-output="nmp2hyd.txt"
-result=$(egrep -v "^#|Status: (Up|Down)" $fname | cut -d' ' -f2,4- | sed -n -e 's/Ignored.*//p' | awk '{for(i=2; i<=NF; i++) { a=a""$i; }; split(a,s,","); for(e in s) { split(s[e],v,"/"); printf "\"%s//%s:%s\"\n" , v[5], $1,v[1]}; a="" ;}' | grep -v "http" | tr '\n' ',' | sed '$ s/.$//' | sed '$s/$/]/' | sed 's/^/[&/'  > $output)
+fname=$1
+output=$2
+result=$(grep -Ev "^#|Status: (Up|Down)" $fname | cut -d' ' -f2,4- | sed -n -e 's/Ignored.*//p' | awk '{for(i=2; i<=NF; i++) { a=a""$i; }; split(a,s,","); for(e in s) { split(s[e],v,"/"); printf "\"%s//%s:%s\"\n" , v[5], $1,v[1]}; a="" ;}' | grep -v "http" | tr '\n' ',' | sed '$ s/.$//' | sed '$s/$/]/' | sed 's/^/[&/'  > $output)
 echo "$result"
