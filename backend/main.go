b16e382bc28bb17b68c07fe32f10c8cb9077ad0e
natthas <32890999+natthas@users.noreply.github.com>
2021-01-04 14:51:39 +0700

    Update main.go

--- backend/main.go
+++ backend/main.go
@@ -3,23 +3,36 @@ package main
 import (
 	//"fmt"
 	//"log"
-	"github.com/gin-gonic/gin"
 	"backendAPI/models"
+
+	"github.com/gin-gonic/gin"
+
 	//"database/sql"
 	_ "github.com/lib/pq"
 	//"context"
+
 	"backendAPI/controllers"
+
 	"github.com/jinzhu/gorm"
+	"github.com/jmoiron/sqlx"
 )
+
 var db *gorm.DB
+var NoSQL *sqlx.DB
 var err error
+
 func main() {
 	r := gin.Default()
 	db := models.ConnectPsql()
+	NoSQL := models.ConnectPNoSQL()
 	r.Use(func(c *gin.Context) {
 		c.Set("db", db)
 		c.Next()
-		})
+	})
+	r.Use(func(c *gin.Context) {
+		c.Set("NoSQL", NoSQL)
+		c.Next()
+	})
 	//r.POST("/AddCompany", controllers.AddNewCompany)
 	//r.POST("/AddAccount", controllers.AddNewAccount)
 	r.GET("/testData/:id", controllers.GetTestData)
@@ -27,8 +40,10 @@ func main() {
 	r.GET("/getVal/:scan_id", controllers.GetVul)
 	r.POST("/addEndpoint", controllers.AddNewEndpoint)
 	r.POST("/addScan", controllers.AddNewScan)
+	r.POST("/RAW", controllers.Raw)
 	r.PATCH("/updateScan/:status", controllers.UpdateScan)
 	r.GET("/getEndpoints/:google_id", controllers.GetEndpoints)
 	r.DELETE("/DeleteEndpoint/:endpoint", controllers.DeleteEndpoint)
-	r.Run() 
+	r.Run()
 }
+
