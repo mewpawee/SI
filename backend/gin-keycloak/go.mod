8dd9b2b82e3506ec3bfbf9de003b33e974387174
ptmew <pawee.tanti@gmail.com>
2021-03-05 12:04:34 +0700

    add gin-keycloak

---
+++ backend/gin-keycloak/go.mod
@@ -0,0 +1,10 @@
+module gin-keycloak
+
+go 1.13
+
+require (
+	github.com/gin-gonic/gin v1.6.3 // indirect
+	github.com/szuecs/gin-glog v1.1.1 // indirect
+	github.com/tbaehler/gin-keycloak v1.1.8 // indirect
+	gopkg.in/go-playground/validator.v8 v8.18.2 // indirect
+)
---
+++ backend/gin-keycloak/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"flag"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	ginglog "github.com/szuecs/gin-glog"
+	"github.com/tbaehler/gin-keycloak/pkg/ginkeycloak"
+)
+
+func main() {
+	flag.Parse()
+	router := gin.New()
+	router.Use(ginglog.Logger(3 * time.Second))
+	router.Use(ginkeycloak.RequestLogger([]string{"uid"}, "data"))
+	router.Use(gin.Recovery())
+
+	var sbbEndpoint = ginkeycloak.KeycloakConfig{
+		Url:   "https://csi.cmkl.ac.th",
+		Realm: "csi",
+	}
+
+	config := ginkeycloak.BuilderConfig{
+		Service: "test",
+		Url:     "https://csi.cmkl.ac.th",
+		Realm:   "csi",
+	}
+	privateGroup := router.Group("/api/privateGroup")
+	privateGroup.Use(ginkeycloak.Auth(ginkeycloak.AuthCheck(), sbbEndpoint))
+	privateGroup.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "test"})
+	})
+	privateUser := router.Group("/api/privateUser")
+	privateUser.Use(ginkeycloak.NewAccessBuilder(config).
+		RestrictButForRealm("broker").
+		Build())
+	privateUser.GET("/", func(c *gin.Context) {
+		uid, okUID := c.Get("uid")
+		if okUID {
+			c.JSON(200, gin.H{"message": uid})
+		}
+	})
+	router.Run()
+	// r := gin.Default()
+	// r.GET("/ping", func(c *gin.Context) {
+	// 	c.JSON(200, gin.H{"message": "ping"})
+	// })
+
+	// r.Run()
+}
