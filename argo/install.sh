62b1ed430745788713960aaba679a168c84d0ad3
Pawee Tantivasdakarn <paweemew@pop-os.localdomain>
2021-04-07 15:36:13 +0700

    fix

---
+++ argo/install.sh
@@ -0,0 +1,2 @@
+kubectl -n argo apply -f https://raw.githubusercontent.com/argoproj/argo-workflows/v2.12.9/manifests/install.yaml
+# kubectl -n argo apply -f https://raw.githubusercontent.com/argoproj/argo-workflows/stable/manifests/install.yaml
