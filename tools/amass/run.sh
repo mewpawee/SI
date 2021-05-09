f4de7c66e4a9b4404e7c2c878f744e496fdbec23
natthas <32890999+natthas@users.noreply.github.com>
2021-05-09 21:17:10 +0700

    Update run.sh

--- tools/amass/run.sh
+++ tools/amass/run.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
-domain = $1
-toNmap="tmp/amass/domain.txt"
-amass enum -active -ip -o $toNmap -d domain
+domain=$1
+toNmap="tmp/domain.txt"
+amass enum -active -ip -o $toNmap -d $domain 2>&1 | tee /tmp/log
