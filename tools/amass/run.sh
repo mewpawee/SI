e9a31d47aa4f03d2f2de17cbba5074e271f63cfb
ptmew <pawee.tanti@gmail.com>
2021-05-22 18:15:08 +0700

    change

--- tools/amass/run.sh
+++ tools/amass/run.sh
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
-#! /bin/bash
-domain=$1
-toNmap="tmp/domain.txt"
-amass enum -active -ip -o $toNmap -d $domain 2>&1 | tee /tmp/log
-=======
 #! /bin/bash
 domain=$1
 jsonForReport="tmp/domain.JSON"
 toNmap="tmp/domain.txt"
 amass enum -active -ip -o $toNmap -json $jsonForReport -d $domain 2>&1 | tee /tmp/log
->>>>>>> amass-image
