3457a67ed07490878dfe1a7970ceab54bc758b07
natthas <32890999+natthas@users.noreply.github.com>
2021-05-24 15:57:08 +0700

    Update main.go

--- backend/main.go
+++ backend/main.go
@@ -98,11 +98,14 @@ func main() {
 	/*r.GET("/testData/:id", controllers.GetTestData)
 	r.GET("/getHost/:scan_id", controllers.GetHost)
 	r.GET("/getVal/:scan_id", controllers.GetVul)
-	r.POST("/addEndpoint", controllers.AddNewEndpoint)
-	r.POST("/addScan", controllers.AddNewScan)*/
-	r.POST("/resultLogs", controllers.Raw)
+	r.POST("/addEndpoint", controllers.AddNewEndpoint)*/
+	r.POST("/addScan", controllers.AddNewScan)
+	r.POST("/uploadResult", controllers.Result)
+	r.POST("/getScanResult", controllers.GetScanResult)
+	//r.POST("/resultLogs", controllers.Raw)
 	/*r.PATCH("/updateScan/:status", controllers.UpdateScan)
 	r.GET("/getEndpoints/:google_id", controllers.GetEndpoints)
 	r.DELETE("/DeleteEndpoint/:endpoint", controllers.DeleteEndpoint)*/
 	r.Run()
 }
+
