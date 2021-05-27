1039d974a5cfc6977c7cac8836c5cf4fab7310e3
natthas <32890999+natthas@users.noreply.github.com>
2021-05-27 20:35:06 +0700

    Update main.go

--- backend/main.go
+++ backend/main.go
@@ -89,6 +89,7 @@ func main() {
 	privateUser.POST("/addEndpoint", controllers.AddEndpoint)       //input poolid, endpoint
 	privateUser.POST("/deleteEndpoint", controllers.DeleteEndpoint) //input endpoint
 	privateUser.GET("/getEndpoints", controllers.GetEndpoints)  //input poolid
+	privateUser.GET("/getAllScans", controllers.GetAllScan) 
 	privateUser.GET("/test", func(c *gin.Context) {
 		c.JSON(200, gin.H{"message": "this thing works"})
 	})
@@ -101,13 +102,11 @@ func main() {
 	r.POST("/addEndpoint", controllers.AddNewEndpoint)*/
 	r.POST("/addScan", controllers.AddNewScan)
 	r.POST("/uploadResult", controllers.Result)
-	r.POST("/getScanResult", controllers.GetScanResult)
-	r.POST("/getScanStatus", controllers.GetScanStatus)
+	r.GET("/getScanResult", controllers.GetScanResult)
+	r.GET("/getScanStatus", controllers.GetScanStatus)
 	//r.POST("/resultLogs", controllers.Raw)
 	r.PATCH("/updateScan/:scan_id", controllers.UpdateScan)
 	/*r.GET("/getEndpoints/:google_id", controllers.GetEndpoints)
 	r.DELETE("/DeleteEndpoint/:endpoint", controllers.DeleteEndpoint)*/
 	r.Run()
 }
-
-
