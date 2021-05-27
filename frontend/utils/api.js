d1973dc01bf43fe50b5f8e2f63d264b57a2fc808
ptmew <pawee.tanti@gmail.com>
2021-05-28 05:14:35 +0700

    another file

--- frontend/utils/api.js
+++ frontend/utils/api.js
@@ -28,3 +28,32 @@ export async function request(method, url, data, auth = false) {
     return e
   }
 }
+
+export async function download(url, data, auth = false) {
+  const headers = {}
+  headers.Accept = '*/*'
+  headers['Content-type'] = 'application/pdf'
+  if (auth) {
+    headers.Authorization = await getToken()
+  }
+  try {
+    // call api
+    const response = await axios({
+      method: 'post',
+      url,
+      headers,
+      data: JSON.stringify(data),
+      responseType: 'blob'
+    }).then((response) => {
+      const url = window.URL.createObjectURL(new Blob([response.data]))
+      const link = document.createElement('a')
+      link.href = url
+      link.setAttribute('download', `${data.scanid}.pdf`)
+      document.body.appendChild(link)
+      link.click()
+    })
+    return response.data
+  } catch (e) {
+    return e
+  }
+}
--- frontend/utils/backendAPI.js
+++ frontend/utils/backendAPI.js
@@ -1,10 +1,30 @@
-import { request } from './api'
+import { request, download } from './api'
+export const downloadReport = async (scanId) => {
+  try {
+    const endpointUrl = 'https://csi.cmkl.ac.th/api/generateReport'
+    const body = {}
+    body.scanid = scanId
+    const result = await download(endpointUrl, body, true)
+    return result
+  } catch (err) {
+    throw new Error(`can't download report`)
+  }
+}
+export const getAllScans = async () => {
+  try {
+    const endpointUrl = `https://csi.cmkl.ac.th/api/privateUser/getAllScans`
+    const result = await request('get', endpointUrl, {}, true)
+    return result
+  } catch (err) {
+    throw new Error('cant retrieve')
+  }
+}
 
 export const getEndpoints = async () => {
   try {
     const endpointsUrl = `https://csi.cmkl.ac.th/api/privateUser/getEndpoints`
-    const test = await request('get', endpointsUrl, {}, true)
-    return test
+    const result = await request('get', endpointsUrl, {}, true)
+    return result
   } catch (err) {
     throw new Error('cant submit')
   }
@@ -12,13 +32,11 @@ export const getEndpoints = async () => {
 
 export const addEndpoint = async (endpoint) => {
   try {
-    const poolResponse = await request('get', poolUrl, {}, true)
-    const poolId = poolResponse.data[0].poolid
     const addEndpointsUrl = `https://csi.cmkl.ac.th/api/privateUser/addEndpoint`
     const body = {}
     body.endpoint = endpoint
-    const test = await request('post', addEndpointsUrl, body, true)
-    return test
+    const result = await request('post', addEndpointsUrl, body, true)
+    return result
   } catch (err) {
     throw new Error('cant submit')
   }
@@ -29,8 +47,8 @@ export const deleteEndpoint = async (endpoint) => {
     const deleteEndpointsUrl = `https://csi.cmkl.ac.th/api/privateUser/deleteEndpoint`
     const body = {}
     body.endpoint = endpoint
-    const test = await request('post', deleteEndpointsUrl, body, true)
-    return test
+    const result = await request('post', deleteEndpointsUrl, body, true)
+    return result
   } catch (err) {
     throw new Error('cant submit')
   }
