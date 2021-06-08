55717ea78d58036616df68d27517e360352edda6
ptmew <pawee.tanti@gmail.com>
2021-06-08 10:58:18 +0700

    filtered dirb

--- tools/filter/main.mjs
+++ tools/filter/main.mjs
@@ -1,42 +1,41 @@
 import fs from "fs";
 import path from "path";
 import { writeJSON } from "./src/function.mjs";
-import { dnsmap, nmap } from "./src/filter.mjs";
+import { dnsmap, nmap, dirb } from "./src/filter.mjs";
+import { DH_NOT_SUITABLE_GENERATOR } from "node:constants";
 
 const readEndpoint = async (dir) => {
   const json = {
-    dnsmap: [],
     nmap: [],
+    dirb: [],
   };
-  const result = await read(dir,json);
+  const result = await read(dir, json);
   for (const key in result) {
     if (result[key].length == 0) {
       result[key] = null;
     }
   }
-  return result
-}
+  return result;
+};
 //read file
 const read = async (dir, json, tool = null) => {
   for await (const d of await fs.promises.opendir(dir)) {
     const entry = path.join(dir, d.name);
     if (d.isDirectory()) {
-      await read(entry, json,d.name);
+      await read(entry, json, d.name);
     } else if (d.isFile()) {
       //console.log("read: " + entry);
       switch (tool) {
-        case "dnsmap":
-          const result = await dnsmap(entry);
-          if (result == null) {
-            json[tool] = null;
-          } else {
-            json.dnsmap.push(result);
+        case "nmap":
+          const nmapResult = await nmap(entry);
+          if (nmapResult) {
+            json.nmap.push(nmapResult);
           }
           break;
-        case "nmap":
-          const test = await nmap(entry);
-          if (test) {
-            json.nmap.push(test);
+        case "dirb":
+          const dirbResult = await dirb(entry);
+          if (dirbResult) {
+            json.dirb.push(dirbResult);
           }
           break;
       }
@@ -46,26 +45,26 @@ const read = async (dir, json, tool = null) => {
 };
 
 // main program
-const main = async (endpoints, company, location = "/mnt/log/") => {
-  const trimedEndpoints = endpoints.replace(/^\[+|\]+$/g, '')
-  const arrayEndpoints = trimedEndpoints.split(',')
+const main = async (company, endpoints, location = "/mnt/log/") => {
+  const trimedEndpoints = endpoints.replace(/^\[+|\]+$/g, "");
+  const arrayEndpoints = trimedEndpoints.split(",");
   const reportObj = {
-    company: company
-  }
-  const endpointsData = []
-  for(const endpoint of arrayEndpoints){
-    const dir = location+endpoint+"/log"
+    company: company,
+  };
+  const endpointsData = [];
+  for (const endpoint of arrayEndpoints) {
+    const dir = location + endpoint + "/log";
     const result = await readEndpoint(dir);
     const endpointObj = {
-      [endpoint] : result
-    }
-    endpointsData.push(endpointObj)
+      [endpoint]: result,
+    };
+    endpointsData.push(endpointObj);
   }
-  reportObj.endpoints = endpointsData
-  console.log(reportObj)
+  reportObj.endpoints = endpointsData;
   // writeJSON("/tmp/report.json", result);
+  writeJSON("./report.json", reportObj);
 };
 
 // print process.argv
 const args = process.argv.slice(2);
-main(args[0],args[1]);
+main(args[0], args[1]);
--- tools/filter/src/filter.mjs
+++ tools/filter/src/filter.mjs
@@ -1,5 +1,6 @@
-import { readJSON, readFile } from "./function.mjs";
+import { readJSON, readFile, command } from "./function.mjs";
 import { nmapFilter } from "./nmap.mjs";
+
 //each tools filter
 
 const dnsmap = async (file) => {
@@ -21,4 +22,25 @@ const nmap = async (file) => {
   return filtered;
 };
 
-export { dnsmap, nmap };
+const dirb = async (file) => {
+  // const cmd = `cat ${file} | awk -F" " '/http/{print $2}'`
+  const cmd = `grep '+' ${file} | sed 's/+ /url-/;s/ (CODE:/,code-/;s/|SIZE:/,size-/;s/)//'`;
+  const grepedData = await command(cmd);
+  const obj = {};
+  if (grepedData) {
+    const data = grepedData.split("\n").slice(0, -1);
+    for (const d of data) {
+      const arr = d.split(",");
+      arr.forEach((string) => {
+        //console.log(arr)
+        const [key, value] = string.split("-");
+        obj[key] = value;
+      });
+    }
+  } else {
+    return undefined;
+  }
+  return obj;
+};
+
+export { dnsmap, nmap, dirb };
