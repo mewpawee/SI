348dbc9aaca59b3018f1c33753074cf4a52498fd
Soponpakorn <60011271@kmitl.ac.th>
2021-06-09 15:53:10 +0700

    fixes nmap part1.2

--- tools/nmap/n2h.sh
+++ tools/nmap/n2h.sh
@@ -8,8 +8,7 @@ result=$(grep -Ev "^#|Status: (Up|Down)" $fname \
         | awk '{for(i=2; i<=NF; i++) { a=a""$i; }; split(a,s,","); for(e in s) { split(s[e],v,"/"); printf "\"%s://%s:%s\"\n" , v[5], $1,v[1]}; a="" ;}' \
         | sed -e 's/?//' -e 's/$/,/' \
         | grep -E "([^.]|^)([0-9]{1,2}|1[0-9]{2}|2[0-4][0-9]|25[0-5]).([0-9]{1,2}|1[0-9]{2}|2[0-4][0-9]|25[0-5]).([0-9]{1,2}|1[0-9]{2}|2[0-4][0-9]|25[0-5]).([0-9]{1,2}|1[0-9]{2}|2[0-4][0-9]|25[0-5])([^.]|$)" \
-        | grep -v 'unknown\|alt' \
+        | grep -v 'unknown\|alt\|sun' \
         | grep 'http\|ssl\|netbios\|ssh\|smtp\|mysql\|rdp\|mongodb\|ncp\|memcached\|irc\|postgres\|rtsp\|sip\|telnet\|vnc\|xmpp' \
-        | cut -d'-' -f3- \
-        | sed -e 's/ssl|http/"https/g' -e '$ s/.$//' -e '1s/"/[&/' -e '$s/$/]/' > $output)
+        | sed -e 's/ssl|http/https/g' -e '$ s/.$//' -e '1s/"/[&/' -e '$s/$/]/' > $output)
 #        | grep -E "(25[0-5]|2[0-4][0-9]|[1]?[1-9][1-9][1-9]?)\.(25[0-5]|2[0-4][0-9]|[1]?[1-9][1-9][1-9]?)\.(25[0-5]|2[0-4][0-9]|[1]?[1-9][1-9][1-9]?)\.(25[0-5]|2[0-4][0-9]|[1]?[1-9][1-9][1-9]?)" > $output)
